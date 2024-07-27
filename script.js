c2bfc25bd1f5162d51fc33d70d73698d256ce65e
Lucas Divo <freirelucas09@gmail.com>
2024-07-27 00:38:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-let cities = ["London", "New York", "Mumbai"];
+rows.push("freeCodeCamp")
 console.log(rows);
